d4ed5d651f8d2c310ab5d91cc25468b2468764f5
RyokCR <ryokwr@gmail.com>
2023-05-03 22:31:23 -0400

    updates plants

--- lib/domain/services/db/admin/initial_plants.dart
+++ lib/domain/services/db/admin/initial_plants.dart
@@ -146,4 +146,168 @@ addPlants(){
       'Toxic',
       'Nutrient Absorption'
   );
+
+  addPlant(
+      'Shadow creeper',
+      '20-40 cm tall',
+      'Small plants found abundantly in the Undergrove area. They are specially adapted to thrive in dark environments,'
+          ' and require very little light to survive.'
+
+      '\n\nThey are known mostly for moving away from direct sources of light in a matter of hours, taking shelter'
+          ' behind nearby plants or objects in order to remain in the shade. They do this by dehydrating and '
+          'reabsorbing one root at a time and repositioning it in the soil, slowly "walking". They often leave fine'
+          ' trails in the soil indicating their trajectory.'
+
+  '\n\nIt is not uncommon for travellers to set up camp in the middle of a clearing, to find most of the plants'
+          ' gone the next morning.',
+      'assets/images/icons/plants/flower_icon.png',
+      0,
+      'https://github.com/RyokCR/Bestiarium/raw/main/content/drawings/Shadow creeper.jpg',
+      'flower',
+      'Locally common',
+      '-',
+      'Photosynthesis'
+  );
+
+
+  addPlant(
+      'Heavensbloom',
+      '2 cm tall',
+      'Exceedingly rare, tiny flowers. Despite their size, their scent is very sweet and strong, able to be picked '
+          'up from quite afar. They grow on unusual surfaces, in any region of Onyria; stone, other plants, living '
+          'creatures and even common house objects. The flowers are so flexible and resilient, they often seem to be '
+          'intangible.'
+
+      '\n\nThere are many legends about Heavensbloom: their smell is said to ward off Kyn\'aeru, and finding a cluster'
+          ' of them is seen as a sign that a God Spirit has taken interest in you. The finder may then ask them for '
+          'guidance, or make a wish.',
+      'assets/images/icons/plants/flower_icon.png',
+      0,
+      'https://github.com/RyokCR/Bestiarium/raw/main/content/drawings/Heavensbloom.jpg',
+      'flower',
+      'Uncommon',
+      '-',
+      'Aeras-based'
+  );
+
+
+  addPlant(
+      'Lepidoxalis',
+      ' 20 cm - 1m diameter clusters',
+      'Beautiful climbing plants that attach themselves to high tree trunks or cliffsides with small, root-like tendrils.'
+          ' Their colorful leaves appear to mimic the shape of a moth\'s wings.'
+
+      '\n\nOnce every few months, some of the individual three-leaved growths develop an adhesive white fuzz at'
+          ' the center, which divides into three separate buds. The leaves grow a single tendril from their midsection,'
+          ' and before long, detach from the plant when a sufficiently strong wind blows.'
+
+  '\n\nThe buds glide away, carried by the wind, and the adhesive growths allow them to stick to any surface they '
+          'hit while flying. The fuzz gives the plant just enough time to affix its tail tendril to the new surface'
+          ' before wilting away, and allowing for a new lepidoxalis cluster to grow.'
+
+  '\n\nThis plant has sometimes been observed growing on large-enough creatures, such as Zelenodon.',
+      'assets/images/icons/plants/other_plants_icon.png',
+      0,
+      'https://github.com/RyokCR/Bestiarium/raw/main/content/drawings/Lepidoxalis.jpg',
+      'other',
+      'Common',
+      '-',
+      'Photosynthesis'
+  );
+
+
+  addPlant(
+      'Evercosmos',
+      '25 cm tall, 10 cm diameter',
+      'Small, delicate flowers that grow in the forests and mild-weathered areas of Onyria. The Evercosmos flower'
+          ' is a symbol of promises, memories and perseverance.'
+
+      '\n\nIt is commonly used as an offering or gift due to its property of retaining colors for many years even it has'
+          ' dried, looking as though the flower was still alive. If not plucked, the flower will be in bloom for most of'
+          ' its existence.'
+
+      '\n\nEvercosmos are highly mutable, and produce blooms of different colors. This is thought to depend on the kind of'
+          ' Aeras energy they have been exposed to throughout their existence, sometimes even changing color or acquiring a'
+          ' combination of two different hues. Other rarer variations of the flower display bioluminscence.'
+
+      '\n\nAs the pigment of the petals is so vivid and durable, they have also been used since ancient times in dyes for'
+          ' clothes, paint or ink.',
+      'assets/images/icons/plants/flower_icon.png',
+      0,
+      'https://github.com/RyokCR/Bestiarium/raw/main/content/drawings/Evercosmos.jpg',
+      'flower',
+      'Common',
+      'Pigmentation',
+      'Photosynthesis'
+  );
+
+  addPlant(
+      'Great Goldveil',
+      '12 m tall',
+      'Large fungi that grow abundantly in the Forest of Bone, which is named as such due to the pale color of the stalks '
+          'and gills of the mushrooms when seen from below.'
+
+      '\n\nThe congregation of Great Goldveil found in the Forest of Bone -linked together below the soil- is '
+          'collectively considered Onyria\'s largest incarnate organism. It is though to have originated from a '
+          'single specimen, spreading for thousands of years.'
+
+      '\n\nOnce the mushrooms grow large enough, they become hardened and resistant to most types of damage, but are'
+          ' fully edible in their first stages of growth.'
+
+  '\n\nThe incredible amount of spores they collectively release into the air however, may be harmful to non-adapted'
+          ' creatures if breathed in.',
+      'assets/images/icons/plants/fungi_icon.png',
+      4,
+      'https://github.com/RyokCR/Bestiarium/raw/main/content/drawings/Great Goldveil.jpg',
+      'fungi',
+      'Locally common',
+      '-',
+      'Nutrient Absorption'
+  );
+
+
+
+  addPlant(
+      'Blue Parasol',
+      '20 cm tall, 15 cm diameter',
+      'Beautiful, but highly toxic mushrooms. They glow in bright cyan hues at night, attracting small arthropodans'
+          ' that become covered in their spores, and consequently spread them.'
+
+      '\n\nBlue parasol grow on the roots of larger plants, feeding off the nutrients they absorb. A large concentration of'
+          ' them, which will often form rings around trees, may result in severe hindering of the host plant\'s growth.'
+
+  '\n\nIts consumption is highly dangerous for small and medium creatures, and may result in death.',
+      'assets/images/icons/plants/fungi_icon.png',
+      3,
+      'https://github.com/RyokCR/Bestiarium/raw/main/content/drawings/Blue Parasol.jpg',
+      'fungi',
+      'Common',
+      'Bioluminescent, poisonous',
+      'Nutrient Absorption'
+  );
+
+
+
+
+  addPlant(
+      'Nightlace',
+      '25 cm',
+      'Vividly colored fungi that grow in shelf-like formations on tree bark, in the tundra forests near the Glacia'
+          ' regions. Its coloration is reminiscent of the hues seen in Onyria\'s night sky.'
+
+  '\n\nThough it is fully edible and not dangerous to consume, Nightlace possesses powerful sleep-inducing properties.'
+          ' It is rumored that even aeru may become drowsy if they were to absorb energy from it.'
+
+  '\n\nNightlace is often dried and ground into a fine powder for consumption. If taken in small amounts, it can ease'
+          ' physical pain.',
+      'assets/images/icons/plants/fungi_icon.png',
+      2,
+      'https://github.com/RyokCR/Bestiarium/raw/main/content/drawings/Blue Parasol.jpg',
+      'fungi',
+      'Locally common',
+      'Narcotic',
+      'Nutrient Absorption'
+  );
+
+
 }
